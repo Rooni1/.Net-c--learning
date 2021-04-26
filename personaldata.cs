f76b0fcb4cb04a84a1c656cc78b3ccd461e23b55
Rooni1 <51120012+Rooni1@users.noreply.github.com>
2021-04-26 10:29:02 +0200

    Add files via upload

---
+++ personaldata.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minauppgift
+{
+     public class personaldata
+    {
+        public string characterName { get; set; }
+        public string opponentName { get; set; }
+        public string health { get; set; }
+        public string Strength { get; set; }
+        public string turn { get; set; }
+
+        public personaldata(string name)
+        {
+            this.characterName = name;
+        }
+    }
+}
